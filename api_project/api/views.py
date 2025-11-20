a32202c21c9a8d968f30d80b21b2fb2fe35d0314
Nana Kwame <owusugabriel803@gmail.com>
2025-11-20 20:42:56 +0000

    api

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,7 @@
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.generics import ListAPIView
+from rest_framework import generics.ListAPIView
 
 class BookList(ListAPIView):
     queryset = Book.objects.all()
