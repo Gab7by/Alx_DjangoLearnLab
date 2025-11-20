4f2445d7679a343d0d7a1ada9ccef5d3d7851223
Nana Kwame <owusugabriel803@gmail.com>
2025-11-20 21:55:52 +0000

    api

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,12 +2,17 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework.generics import ListAPIView
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 from rest_framework import generics.ListAPIView
+
 
 class BookList(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
+    
 class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book.object.all()
+    queryset = Book.objects.all()
     serializer_class = BookSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
