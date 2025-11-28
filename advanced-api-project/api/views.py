aa22116f12f341a18fce5c3610f0d480d4259a38
Nana Kwame <owusugabriel803@gmail.com>
2025-11-28 14:54:07 +0000

    advanved

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
@@ -8,6 +8,20 @@ class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.AllowAny]
+    
+    filter_backends = [
+        'DjangoFilterBackend',
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+    
+    ilterset_fields = ['title', 'publication_year', 'author']
+
+    search_fields = ['title', 'author__name']
+
+    ordering_fields = ['title', 'publication_year']
+    
+    ordering = ['title']
     
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
