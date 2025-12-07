130262304cbc743e92c93cd19a4074e81a327b29
Nana Kwame <owusugabriel803@gmail.com>
2025-12-07 09:25:08 +0000

    django blog update 1

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -69,14 +69,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_blog.wsgi.application'
 
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'root',
+        'PASSWORD': 'Mylove6711',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
