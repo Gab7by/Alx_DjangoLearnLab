0d191d30166f57d804e1f06e5d3c68bf179f50e4
Nana Kwame <owusugabriel803@gmail.com>
2025-12-17 20:24:22 +0000

    project 5

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,40 +1,44 @@
-from django.contrib.auth import get_user_model
 from django.contrib.auth import authenticate
-from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
-class RegisterSerializers(serializers.ModelSerializer):
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
-        fields = ["username", "email", "password", "bio", "profile_picture"]
-    
+        fields = ["username", "email", "password"]
+
     def create(self, validated_data):
-        user = User.objects.create_user(
-            username = validated_data.get("username"),
-            email = validated_data["email"],
-            password = validated_data["password"],
-            bio = validated_data.get("bio", ""),
-            profile_picture = validated_data.get("profile_picture"),
+        user = get_user_model().objects.create_user(
+            username=validated_data["username"],
+            email=validated_data.get("email"),
+            password=validated_data["password"],
         )
+
+        Token.objects.create(user=user)
         return user
-    
+
+
 class LoginSerializer(serializers.Serializer):
-    email = serializers.EmailField(blank = False, null=False)
-    password = serializers.CharField(write_only = True)
-    
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
     def validate(self, data):
         user = authenticate(
-            email=data["email"],
+            username=data["username"],
             password=data["password"]
         )
-        
+
         if not user:
             raise serializers.ValidationError("Invalid credentials")
-        
-        token, _ = Token.objects.get_or_create(user=user)
-        
+
+        token = Token.objects.create(user=user)
+
         return {
             "user": user,
             "token": token.key
