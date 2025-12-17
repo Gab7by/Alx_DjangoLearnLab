fc2545499d2636f954dea90593d458bce71d9399
Nana Kwame <owusugabriel803@gmail.com>
2025-12-17 21:10:29 +0000

    project8

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -63,7 +63,7 @@ class FeedView(APIView):
 
         posts = Post.objects.filter(
             author__in=following_users
-        ).order_by("-created_at")
+        ).order_by
 
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
